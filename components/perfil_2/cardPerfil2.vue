5c94b8ebdfdb7632af93d2d285b460cb75cf4862
ivmendoz <ivmtrab183@gmail.com>
2025-05-23 10:02:24 -0400

    Cambios

--- components/perfil_2/cardPerfil2.vue
+++ components/perfil_2/cardPerfil2.vue
@@ -57,7 +57,7 @@
                 type="button"
                 class="text-white bg-purple-700 hover:bg-purple-800 focus:outline-none focus:ring-4 focus:ring-purple-300 font-medium rounded-full text-sm px-5 py-2.5 text-center dark:bg-purple-600 dark:hover:bg-purple-700 dark:focus:ring-purple-800"
               >
-                Agregar usuario
+                Agregar usuarioooooooooooo
               </button>
             </div>
           </div>
